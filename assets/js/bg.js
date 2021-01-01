aa0648a01ef075110cd79f67fa2b031a50350b64
Henry, Lim (Seonghyeon) <69956570+limunosekai@users.noreply.github.com>
2021-01-01 23:27:55 +0900

    Update bg.js

--- assets/js/bg.js
+++ assets/js/bg.js
@@ -4,7 +4,7 @@ const IMG_NUMBER = 3;
 
 function paintImage(imgNumber) {
   const image = new Image();
-  image.src = `/../../images/${imgNumber}.jpg`;
+  image.src = `../../images/${imgNumber}.jpg`;
   image.classList.add('bgImage');
   body.appendChild(image);
 }
